0923b45c3bd9dff61397a20b44d1b69e70c0346d
jawaheralaggl <jawaheralaggl@gmail.com>
2020-07-21 11:28:26 +0300

    first commit

--- LocalNotification/AppDelegate.swift
+++ LocalNotification/AppDelegate.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("User Confirm")
+            }else {
+                print("Error")
+            }
+        }
         return true
     }
 
@@ -31,7 +40,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    
+     //MARK:- UNUSerNotification
+        
+        func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            
+            completionHandler([.alert, .badge, .sound])
+        }
+        
+        func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+            completionHandler()
+        }
+    }
 
 
-}
+
 
--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    @IBAction func setReminder(_ sender: Any) {
+        
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        content.title = "This is a reminder!"
+        content.body = "Check Now!!"
+        content.sound = .default
+        
+        
+        // Set Custom Action
+        let categoryIdentifier = "reminder.action"
+        let check = UNNotificationAction(identifier: "do.check", title: "Check", options: [.foreground])
+        let cancel = UNNotificationAction(identifier: "do.cancel", title: "Cancel", options: [])
+        let category = UNNotificationCategory(identifier: categoryIdentifier, actions: [check, cancel], intentIdentifiers: [], options: [])
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+        
+        content.categoryIdentifier = categoryIdentifier
+        
+        
+        let notiDate = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute, .second], from: Date().addingTimeInterval(10))
+        // let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 20, repeats: false)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: notiDate, repeats: true)
+        let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
+        center.add(request) { (error) in
+            if error != nil {
+                print("error:\(error?.localizedDescription ?? "Error Local Notification" )")
+            }
+        }
+        
+   
+    }
 }
+
 
